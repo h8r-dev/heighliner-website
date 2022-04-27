89a68d23ad7f3fb6957892652a36ccc748e49186
zhangze <1183460943@qq.com>
2022-04-27 17:11:45 +0800

    add: complate JoinCommunity session

--- src/components/homepage/JoinCommunity/index.tsx
+++ src/components/homepage/JoinCommunity/index.tsx
@@ -2,38 +2,51 @@
  * Create by ze.zhang on 2022.4.27
  */
 import React from "react";
+import {customFields} from "@site/docusaurus.config";
 
 import styles from './index.module.css';
 
 interface IconType {
   src: string,
   alt: string,
+  link?: string,
 }
 
 const iconList: IconType[] = [
   {
-    src: require('@site/static/img/homepage/heighliner-innovation-logo.png').default,
-    alt: 'github'
+    src: require('@site/static/img/homepage/joincommunity/github.webp').default,
+    alt: 'github',
+    link: customFields.githubUrl as string
   },
-  {
-    src: require('@site/static/img/homepage/k8s-logo.png').default,
-    alt: 'discord',
-  },
-  {
-    src: require('@site/static/img/homepage/terraform-logo.png').default,
-    alt: 'wechat',
-  },
+  // annotate discord and wechat link
+  // {
+  //   src: require('@site/static/img/homepage/joincommunity/discourd.webp').default,
+  //   alt: 'discord',
+  // },
+  // {
+  //   src: require('@site/static/img/homepage/joincommunity/wechat.webp').default,
+  //   alt: 'wechat',
+  // },
 ]
 
 export function JoinCommunity() {
+
+  function hendleClick(link: (string | undefined)) {
+    link && window.open(link)
+  }
+
   return (
-    <div >
+    <div>
       <div className={styles.title}>Join the community</div>
       <div className={styles.iconWrapper}>
         {
-          iconList.map(item => {
-            let {src, alt} = item;
-            return <img src={src} alt={alt} className={styles.icon}/>
+          iconList.map((item: IconType, key: number) => {
+            let {src, alt, link} = item;
+            return (
+              <div className={styles.icon} key={key}>
+                <img src={src} alt={alt} onClick={() => hendleClick(link)}/>
+              </div>
+            )
           })
         }
       </div>
