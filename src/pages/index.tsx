203b9502289d48c6aec61e66a789f63af7281627
zhangze <1183460943@qq.com>
2022-04-29 15:07:35 +0800

    style: add global table mask

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,19 +7,13 @@ import React from "react";
 import Head from '@docusaurus/Head'
 
 import { SloganSection } from "../components/homepage/SloganSection";
-import { HeighlinerDetail } from "../components/homepage/HeighlinerDetail";
-import { HeighlinerFeatures } from "../components/homepage/HeighlinerFeatures";
 import { WhyHeighliner } from "../components/homepage/WhyHeighliner";
 import { StackWorkflow } from "../components/homepage/StackWorkflow";
 import { ChooseStack } from "../components/homepage/ChooseStacks";
-import { Innovation } from "../components/homepage/Innovation";
 import { JoinCommunity } from "../components/homepage/JoinCommunity";
 import { ConnectAny } from "../components/homepage/ConnectAny";
 import { Community } from "@site/src/components/homepage/Community";
 import { Footer } from "../components/homepage/Footer";
-
-import { Redirect } from '@docusaurus/router';
-import { customFields } from "@site/docusaurus.config";
 
 import styles from './index.module.scss'
 
@@ -31,13 +25,12 @@ export default function Home(): JSX.Element {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minium-scale=1.0, user-scalable=no" />
       </Head>
       <SloganSection />
-      {/* <HeighlinerDetail /> */}
       <WhyHeighliner />
-      {/* <HeighlinerFeatures /> */}
-      <StackWorkflow />
-      <ChooseStack />
-      {/*<Innovation />*/}
-      <ConnectAny />
+      <div className={styles.tableMask}>
+        <StackWorkflow />
+        <ChooseStack />
+        <ConnectAny />
+      </div>
       <Community />
       <JoinCommunity />
       <Footer />
