af411259850be269033f0bee450dd30e2a80c8c8
ChenYuan <cy476571@gmail.com>
2022-05-05 12:25:05 +0800

    refactor asciinema module

--- src/components/homepage/StackWorkflow/Step.tsx
+++ src/components/homepage/StackWorkflow/Step.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-
+import React, { useRef, useEffect } from "react";
 import "asciinema-player/dist/bundle/asciinema-player.css"
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
 import styles from "./index.module.scss";
 
 export interface StepProps {
@@ -9,12 +10,27 @@ export interface StepProps {
   id: string
 }
 
-export default function Step({ title, id }: StepProps): React.ReactElement {
+export default function Step({ title, id, asciiData }: StepProps): React.ReactElement {
+  const terminalRef = useRef(null)
+  useEffect(() => {
+    if (ExecutionEnvironment.canUseDOM) {
+      import('asciinema-player').then(player => {
+        player.create(`data:text/plain;base64,${asciiData}`, terminalRef.current, {
+          loop: true,
+          autoPlay: true,
+          terminalFontSize: '14px',
+          fit: false,
+          theme: 'asciinema',
+          rows: 19,
+        })
+      })
+    }
+  }, [])
 
   return (
     <div className={styles.stepWrap}>
       <div className={styles.stepTitle}>{title}</div>
-      <div className={styles.terminal} id={id}/>
+      <div ref={terminalRef} className={styles.terminal} id={id}/>
     </div>
   );
 }
