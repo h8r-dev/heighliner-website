c775684b972ae680e852de5ceb1bd5535b8c1e9d
Pandy <wkpcoder@163.com>
2022-05-11 14:31:16 +0800

    Feat: Features responsible

--- src/components/homepage/WhyHeighliner/Feature.tsx
+++ src/components/homepage/WhyHeighliner/Feature.tsx
@@ -15,15 +15,28 @@ export default function Feature({
   description,
 }: Props): React.ReactElement {
   return (
-    <div className={styles.featureContain}>
-      <div className={styles.featureTitle}>{title}</div>
-      <img
-        src={imgPath}
-        alt="with heighliner"
-        style={{ width: 150, height: 150 }}
-        className={styles.featureImg}
-      />
-      <div className={styles.featureDescription}>{description}</div>
-    </div>
+    <>
+      <div className={styles.featureContain}>
+        <div className={styles.featureTitle}>{title}</div>
+        <img
+          src={imgPath}
+          alt="with heighliner"
+          style={{ width: 150, height: 150 }}
+          className={styles.featureImg}
+        />
+        <div className={styles.featureDescription}>{description}</div>
+      </div>
+      <div className={clsx(styles.featureContain, styles.md)}>
+        <img
+          src={imgPath}
+          alt="with heighliner"
+          className={styles.featureImg}
+        />
+        <div className={styles.featureContent}>
+          <div className={styles.featureTitle}>{title}</div>
+          <div className={styles.featureDescription}>{description}</div>
+        </div>
+      </div>
+    </>
   );
 }
