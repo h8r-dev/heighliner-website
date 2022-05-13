9ad7f4b5da39daf66af6047c166ae4b9e6c11da9
Pandy <wkpcoder@163.com>
2022-05-13 18:50:08 +0800

    Feat: Heighliner Header Component (#64)

---
+++ src/components/Header/HeighlinerHeaderBg/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+interface Props {
+  children?: React.ReactElement
+}
+
+export default function HeighlinerHeader({ children }: Props): React.ReactElement {
+  return (
+    <div className={styles.BgWrapper}>
+      <img
+        src={
+          require("/img/component/HeighlinerHeader/header-background.webp")
+          .default
+        }
+        alt="with heighliner"
+        />
+      <div className={styles.main}>
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/contact-us.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import HeighlinerHeader from "../components/Header/HeighlinerHeader";
+import HeighlinerHeaderBg from "../components/Header/HeighlinerHeaderBg";
+
+export default function ContactUs(): React.ReactElement {
+  return (
+    <HeighlinerHeaderBg>
+      <HeighlinerHeader />
+    </HeighlinerHeaderBg>
+  );
+}
