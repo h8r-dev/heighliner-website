b03916743727d43a21fcd6bc0d96425b0503b0b2
Pandy <wkpcoder@163.com>
2022-05-15 17:14:07 +0800

    Feat: directly load .cast file (#66)

--- src/components/Asciinema/index.tsx
+++ src/components/Asciinema/index.tsx
@@ -3,36 +3,37 @@
  */
 
 import React, { useRef, useEffect } from "react";
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
-import "asciinema-player/dist/bundle/asciinema-player.css"
+import "asciinema-player/dist/bundle/asciinema-player.css";
 
 // import styles from "./index.module.scss";
 
 export interface AsciinemaPlayerProps {
-  asciiData: string,
-  className?: string,
+  castFileUrl: URL;
+  className?: string;
 }
 
-export default function AsciinemaPlayer({ asciiData, className = '' }: AsciinemaPlayerProps) {
-  const asciinemaRef = useRef(null)
+export default function AsciinemaPlayer({
+  castFileUrl,
+  className = "",
+}: AsciinemaPlayerProps) {
+  const asciinemaRef = useRef(null);
 
   useEffect(() => {
     if (ExecutionEnvironment.canUseDOM) {
-      import('asciinema-player').then(player => {
-        player.create(`data:text/plain;base64,${asciiData}`, asciinemaRef.current, {
+      import("asciinema-player").then((player) => {
+        player.create(castFileUrl, asciinemaRef.current, {
           loop: true,
           autoPlay: true,
-          terminalFontSize: '14px',
+          terminalFontSize: "14px",
           fit: false,
-          theme: 'asciinema',
+          theme: "asciinema",
           rows: 19,
-        })
-      })
+        });
+      });
     }
-  }, [])
+  }, []);
 
-  return (
-    <div ref={asciinemaRef} className={className}></div>
-  )
+  return <div ref={asciinemaRef} className={className}></div>;
 }
