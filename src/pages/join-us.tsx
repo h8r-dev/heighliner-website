4d7b9474d485d6bbb5f7278a397e9fca323a8f1f
zhangze <1183460943@qq.com>
2022-05-16 16:35:34 +0800

    fix: add list key

--- src/pages/join-us.tsx
+++ src/pages/join-us.tsx
@@ -98,74 +98,74 @@ export const list = [
 export default function AboutUs(): JSX.Element {
   return (
     <PageWrapper title="Join us">
-        <div className={styles.wrapper}>
-          <div className={styles.pageTitle}>Join Heighliner and build the future of Cloud Native</div>
-          <div className={styles.pageTitleSecond}>About Heighliner</div>
-          <div className={styles.pageDesc}>
-            Heighliner is a modern app development platform.
-            It aims to help developers to build apps easily and quickly using state-of-the-art cloud native stacks.
-            You can use Heighliner to spin up the development environment,
-            and then can focus on writing the business code without worrying build and deploy.
-          </div>
-          <div className={styles.pageDesc}>
-            The core team of Heighliner comes from
-            &nbsp;<a href="https://nocalhost.dev" target="_blank">https://nocalhost.dev</a>
-            &nbsp;<a href="https://kubevela.io" target="_blank">https://kubevela.io</a> .
-          </div>
-          <div className={styles.pageDesc}>
-            We are a young team, advocating freedom and openness,
-            and have gathered a group of small partners who are full of curiosity,
-            love open source, and dream of using technology to change the world.
-            We aim to use the most advanced cloud-native open source technology,
-            so that users don't need to care about infrastructure,
-            get a better development experience and the advantages of multi-cloud delivery,
-            cost reduction and efficiency improvement.
-          </div>
-          {
-            list.map((i) => {
-              let {title, desc, item, icon} = i;
-              return (
-                <div className={styles.hc}>
-                  <img src={icon} alt="" className={styles.hcIcon}/>
-                  <div className={styles.hcTitle}>{title}</div>
-                  <div className={styles.hcDesc}>{desc}</div>
-                  {
-                    item.map(j => {
-                      let {itemTitle, itemList} = j;
-                      return (
-                        <>
-                          <div className={styles.hcItemTitle}>{itemTitle}</div>
-                          {
-                            itemList.map(h => <div className={styles.hcItemDesc}>· {h}</div>)
-                          }
-                        </>
-                      )
-                    })
-                  }
-                  <div className={styles.hcEmail}>Send us an email to apply this position:
-                    <a href={"mailto:" + siteConfig.customFields.email}> {siteConfig.customFields.email}</a>
-                  </div>
-                </div>
-              )
-            })}
-          <div className={styles.positionTitle}>Our Opening Positions</div>
-          {
-            list.map((i, index) => {
-              let {title} = i;
-              return (
-                <div className={styles.hcBox}>
-                  <div className={styles.hcBoxTitle}>{title}</div>
-                  <Link to={`./hc-detail?index=${index}`}>
-                    <div className={styles.hcBoxApply}>
-                      <img src={require('@site/static/img/join-us/apply.webp').default} alt=""/>
-                      <span>Apply here</span>
-                    </div>
-                  </Link>
-                </div>
-              )
-            })
-          }
+      <div className={styles.wrapper}>
+        <div className={styles.pageTitle}>Join Heighliner and build the future of Cloud Native</div>
+        <div className={styles.pageTitleSecond}>About Heighliner</div>
+        <div className={styles.pageDesc}>
+          Heighliner is a modern app development platform.
+          It aims to help developers to build apps easily and quickly using state-of-the-art cloud native stacks.
+          You can use Heighliner to spin up the development environment,
+          and then can focus on writing the business code without worrying build and deploy.
         </div>
+        <div className={styles.pageDesc}>
+          The core team of Heighliner comes from
+          &nbsp;<a href="https://nocalhost.dev" target="_blank">https://nocalhost.dev</a>
+          &nbsp;<a href="https://kubevela.io" target="_blank">https://kubevela.io</a> .
+        </div>
+        <div className={styles.pageDesc}>
+          We are a young team, advocating freedom and openness,
+          and have gathered a group of small partners who are full of curiosity,
+          love open source, and dream of using technology to change the world.
+          We aim to use the most advanced cloud-native open source technology,
+          so that users don't need to care about infrastructure,
+          get a better development experience and the advantages of multi-cloud delivery,
+          cost reduction and efficiency improvement.
+        </div>
+        {
+          list.map((i) => {
+            let {title, desc, item, icon} = i;
+            return (
+              <div className={styles.hc} key={title}>
+                <img src={icon} alt="" className={styles.hcIcon}/>
+                <div className={styles.hcTitle}>{title}</div>
+                <div className={styles.hcDesc}>{desc}</div>
+                {
+                  item.map(j => {
+                    let {itemTitle, itemList} = j;
+                    return (
+                      <div key={itemTitle}>
+                        <div className={styles.hcItemTitle}>{itemTitle}</div>
+                        {
+                          itemList.map(h => <div className={styles.hcItemDesc} key={h}>· {h}</div>)
+                        }
+                      </div>
+                    )
+                  })
+                }
+                <div className={styles.hcEmail}>Send us an email to apply this position:
+                  <a href={"mailto:" + siteConfig.customFields.email}> {siteConfig.customFields.email}</a>
+                </div>
+              </div>
+            )
+          })}
+        <div className={styles.positionTitle}>Our Opening Positions</div>
+        {
+          list.map((i, index) => {
+            let {title} = i;
+            return (
+              <div className={styles.hcBox} key={index}>
+                <div className={styles.hcBoxTitle}>{title}</div>
+                <Link to={`./hc-detail?index=${index}`}>
+                  <div className={styles.hcBoxApply}>
+                    <img src={require('@site/static/img/join-us/apply.webp').default} alt=""/>
+                    <span>Apply here</span>
+                  </div>
+                </Link>
+              </div>
+            )
+          })
+        }
+      </div>
     </PageWrapper>
   );
 }
