6bb33a3efdf2593daf0814410546a1f3c5e038b5
Pandy <wkpcoder@163.com>
2022-05-17 16:49:42 +0800

    Feat: homepage use header component (#77)
    * Feat: homepage use header component
    
    * Fix: delete useless break
    
    * Fix: header to big

--- src/components/homepage/SloganSection/index.tsx
+++ src/components/homepage/SloganSection/index.tsx
@@ -7,42 +7,21 @@ import Link from "@docusaurus/Link";
 import clsx from "clsx";
 import siteConfig from "@generated/docusaurus.config";
 
-
-import WhiteHeighlinerSvg from "@site/static/img/homepage/white-heighliner.svg";
-import PurpleHeighlinerSVG from "@site/static/img/homepage/slogansection/purple-heighliner.svg"
 import { Video } from "@site/src/components/Video";
-
 
 import styles from "./index.module.scss";
 import AutoDownCounter from "./AutoDownCounter";
-import NavItems from "./NavItems";
+import HeighlinerHeader from "../../Header/HeighlinerHeader";
 
 export function SloganSection(): React.ReactElement {
-  const [whiteHeader, setWhiteHeader] = useState<typeof styles>({});
-
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.backgroundMask} />
       <div className={styles.container}>
         <div className="slogan-section container">
-          <header className={whiteHeader}>
-            <nav className={styles.navbar}>
-              <div className={styles.logoWrap}>
-                <img
-                  src={
-                    require("@site/static/img/homepage/slogansection/purple-logo@3x.webp")
-                      .default
-                  }
-                  style={{ marginRight: 18, width: 51, height: 33 }}
-                  alt="with heighliner"
-                />
-                <WhiteHeighlinerSvg className={styles.whiteHeighliner} />
-                <PurpleHeighlinerSVG className={styles.purpleHeighliner} />
-              </div>
-              <NavItems setWhiteHeader={setWhiteHeader} />
-            </nav>
-          </header>
+          <div className={styles.verticallyFullOfScreen}>
+            <HeighlinerHeader />
+          </div>
           <div className={styles.contentWrap}>
             <main className={styles.wrap}>
               <div className={styles.sloganTextWrap}>
