ee70f4888cce5767fffb4151286d9eafe572111b
Pandy <wkpcoder@163.com>
2022-05-19 14:29:47 +0800

    Hotfix: print commit info

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -9,6 +9,9 @@ OUTPUT_FILE_PATE="./plugins/ConsoleBuildInfo.bak.mjs"
 COMMIT_HASH=$(git rev-parse HEAD)
 BUILD_TIME=$(date)
 
+echo "Commit hash: $COMMIT_HASH"
+echo "Build time: $BUILD_TIME \n"
+
 sed -e "s/COMMIT_HASH/\"$COMMIT_HASH\"/" \
   -e "s/BUILD_TIME/\"$BUILD_TIME\"/" \
   $INPUT_FILE_PATH >$OUTPUT_FILE_PATE
