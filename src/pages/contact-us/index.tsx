2e4d2d11c5adca7326c0e3258456ebef2f23208d
Pandy <wkpcoder@163.com>
2022-05-26 20:57:42 +0800

    Feat: add sendgrid

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Entries, { EntriesType } from "../../components/Entries";
-
-import PageWrapper from "../../components/PageWrapper";
-
 import siteConfig from "@generated/docusaurus.config";
+
+import Entries, { EntriesType } from "../../components/Entries";
+import PageWrapper from "../../components/PageWrapper";
+import styles from "./index.module.scss";
 
 const entries: EntriesType = [
   {
@@ -15,7 +15,11 @@ const entries: EntriesType = [
     title: "Open an issue on GitHub",
     content: (
       <>
-        Create an issue on our <a href={siteConfig.customFields.githubUrl as string} target="_blank">Github repo</a>.
+        Create an issue on our{" "}
+        <a href={siteConfig.customFields.githubUrl as string} target="_blank">
+          Github repo
+        </a>
+        .
       </>
     ),
   },
@@ -23,7 +27,10 @@ const entries: EntriesType = [
     title: "Join Heighliner User Group on Discord",
     content: (
       <>
-        Join our <a href={siteConfig.customFields.discordUrl as string} target="_blank">Discord channel</a>
+        Join our{" "}
+        <a href={siteConfig.customFields.discordUrl as string} target="_blank">
+          Discord channel
+        </a>
       </>
     ),
   },
@@ -31,9 +38,10 @@ const entries: EntriesType = [
     title: "Follow Heighliner's Twitter",
     content: (
       <>
-        Follow us on <a href={siteConfig.customFields.twitterUrl as string}>Twitter</a>
+        Follow us on{" "}
+        <a href={siteConfig.customFields.twitterUrl as string}>Twitter</a>
       </>
-    )
+    ),
   },
   {
     title: "Join Heighliner User Group on WeChat",
@@ -41,23 +49,29 @@ const entries: EntriesType = [
   },
   {
     title: "Follow Heighliner's WeChat Official Account",
-    imgPath: require('/img/page/contact-us/WeChatChannel.png').default,
+    imgPath: require("/img/page/contact-us/WeChatChannel.png").default,
   },
   {
     title: "Email",
-    content:
-      (
-        <>
-          If you have any questions, don't hesitate to contact us at contact <a href={"mailto:" + siteConfig.customFields.contactEmail}>{siteConfig.customFields.contactEmail}</a>
-        </>
-      )
+    content: (
+      <>
+        If you have any questions, don't hesitate to contact us at contact{" "}
+        <a href={"mailto:" + siteConfig.customFields.contactEmail}>
+          {siteConfig.customFields.contactEmail}
+        </a>
+      </>
+    ),
   },
 ];
 
 export default function Index(): React.ReactElement {
   return (
     <PageWrapper title="Contact us">
-      <Entries entries={entries} />
+      {/* <Entries entries={entries} /> */}
+      <iframe
+        src="https://cdn.forms-content.sg-form.com/73f88cd2-dccb-11ec-8e46-52816f0f79c2"
+        className={styles.sendgrid}
+      />
     </PageWrapper>
   );
 }
