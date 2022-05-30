a6eb13fedacb36261abc102e969bfd3d0b2b0ffe
Pandy <wkpcoder@163.com>
2022-05-30 13:21:45 +0800

    Feat: contact us v2.0

---
+++ src/components/MediaMatrix/index.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+import siteConfig from "@generated/docusaurus.config";
+
+export default function (): React.ReactElement {
+  return (
+    <div className={styles.mediaMatrix}>
+      <ul>
+        <li>
+          <a href={siteConfig.customFields.twitterUrl as string}>
+            <img
+              src={require("/img/page/contact-us/Twitter@3x.png").default}
+              alt="without Heighliner"
+            />
+          </a>
+        </li>
+        <li>
+          <a href={siteConfig.customFields.githubUrl as string}>
+            <img
+              src={require("/img/page/contact-us/GitHub@3x.png").default}
+              alt="without Heighliner"
+            />
+          </a>
+        </li>
+        <li>
+          <div className={styles.wechat}>
+            <img
+              src={require("/img/page/contact-us/WeChat@3x.png").default}
+              alt="without Heighliner"
+            />
+            <div className={styles.qrCodes}>
+              <div className={styles.pairs}>
+                <div className={styles.pair}>
+                  <img
+                    src="/img/page/contact-us/wechatPersonalAccount@3x.png"
+                    alt="Without Heighliner"
+                  />
+                  <p>Join Heighliner's User Group on WeChat.</p>
+                </div>
+                <div className={styles.pair}>
+                  <img
+                    src="/img/page/contact-us/wechatOfficialAccount@3x.png"
+                    alt="Without Heighliner"
+                  />
+                  <p>Fellow Heighliner's WeChat Official Account.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <a href={siteConfig.customFields.discordUrl as string}>
+            <img
+              src={require("/img/page/contact-us/Discord@3x.png").default}
+              alt="without Heighliner"
+            />
+          </a>
+        </li>
+        <li>
+          <a href={siteConfig.customFields.youTubeUrl as string}>
+            <img
+              src={require("/img/page/contact-us/YouTube@3x.png").default}
+              alt="without Heighliner"
+            />
+          </a>
+        </li>
+        <li>
+          <a href={siteConfig.customFields.linkedInUrl as string}>
+            <img
+              src={require("/img/page/contact-us/LinkedIn@3x.png").default}
+              alt="without Heighliner"
+            />
+          </a>
+        </li>
+        <li>
+          <a href={siteConfig.customFields.bilibiliUrl as string}>
+            <img
+              src={require("/img/page/contact-us/Bilibili@3x.png").default}
+              alt="without Heighliner"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,77 +1,31 @@
 import React from "react";
-import siteConfig from "@generated/docusaurus.config";
 
-import Entries, { EntriesType } from "../../components/Entries";
-import PageWrapper from "../../components/PageWrapper";
+import PageWrapper from "@site/src/components/PageWrapper";
+import MediaMatrix from "@site/src/components/MediaMatrix";
+
+import SendGridForm from "./SendGridForm";
 import styles from "./index.module.scss";
+import Title from "./Title";
 
-const entries: EntriesType = [
-  {
-    content:
-      "Got questions? Have feedback? " +
-      "Here are a couple of official channels to get in touch with us.",
-  },
-  {
-    title: "Open an issue on GitHub",
-    content: (
-      <>
-        Create an issue on our{" "}
-        <a href={siteConfig.customFields.githubUrl as string} target="_blank">
-          Github repo
-        </a>
-        .
-      </>
-    ),
-  },
-  {
-    title: "Join Heighliner User Group on Discord",
-    content: (
-      <>
-        Join our{" "}
-        <a href={siteConfig.customFields.discordUrl as string} target="_blank">
-          Discord channel
-        </a>
-      </>
-    ),
-  },
-  {
-    title: "Follow Heighliner's Twitter",
-    content: (
-      <>
-        Follow us on{" "}
-        <a href={siteConfig.customFields.twitterUrl as string}>Twitter</a>
-      </>
-    ),
-  },
-  {
-    title: "Join Heighliner User Group on WeChat",
-    imgPath: require("/img/page/contact-us/HeightLinerWeChat.jpg").default,
-  },
-  {
-    title: "Follow Heighliner's WeChat Official Account",
-    imgPath: require("/img/page/contact-us/WeChatChannel.png").default,
-  },
-  {
-    title: "Email",
-    content: (
-      <>
-        If you have any questions, don't hesitate to contact us at contact{" "}
-        <a href={"mailto:" + siteConfig.customFields.contactEmail}>
-          {siteConfig.customFields.contactEmail}
-        </a>
-      </>
-    ),
-  },
-];
-
-export default function Index(): React.ReactElement {
+export default function (): React.ReactElement {
   return (
-    <PageWrapper title="Contact us">
-      {/* <Entries entries={entries} /> */}
-      <iframe
-        src="https://cdn.forms-content.sg-form.com/73f88cd2-dccb-11ec-8e46-52816f0f79c2"
-        className={styles.sendgrid}
-      />
+    <PageWrapper
+      title="Contact us"
+      customSecondaryBottom={<MobileContactUsSection />}
+    >
+      <SendGridForm />
     </PageWrapper>
+  );
+}
+
+function MobileContactUsSection() {
+  return (
+    <div className={styles.mobileContactUsSection}>
+      <img src="/img/page/contact-us/wave@3x.webp" alt="without Heighliner" />
+      <div className={styles.content}>
+        <Title />
+        <MediaMatrix />
+      </div>
+    </div>
   );
 }
