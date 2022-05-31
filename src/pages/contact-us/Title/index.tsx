10c4f12f0c95ae07b925ba6d7300df9f0b243264
Pandy <wkpcoder@163.com>
2022-05-31 16:18:59 +0800

    Feat: Refactor Contact us page (#126)
    * Feat: Doc page v2 (#116)
    
    * Feat: Docs page v2
    
    * Feat: change default mode to light
    
    * fix: logo show in light mode (#117)
    
    * Feat: Contact us v2 (#119)
    
    * Feat: Doc page v2 (#116)
    
    * Feat: Docs page v2
    
    * Feat: change default mode to light
    
    * fix: logo show in light mode (#117)
    
    * Feat: add sendgrid
    
    * Feat: contact us v2.0
    
    * Feat/pandy/contact us v2 (#121)
    
    * Feat: Doc page v2 (#116)
    
    * Feat: Docs page v2
    
    * Feat: change default mode to light
    
    * fix: logo show in light mode (#117)
    
    * Feat: add sendgrid
    
    * Feat: contact us v2.0
    
    * Feat: delete subject
    
    * Fix: Add more field in contact us (#123)
    
    * Feat: Doc page v2 (#116)
    
    * Feat: Docs page v2
    
    * Feat: change default mode to light
    
    * fix: logo show in light mode (#117)
    
    * Fix: add more fields in contact us page
    
    * Fix: increase input height (#124)
    
    * Feat: Doc page v2 (#116)
    
    * Feat: Docs page v2
    
    * Feat: change default mode to light
    
    * fix: logo show in light mode (#117)
    
    * fix: justify input heigh
    
    * Fix: Remove asterisk (#125)
    
    * Feat: Doc page v2 (#116)
    
    * Feat: Docs page v2
    
    * Feat: change default mode to light
    
    * fix: logo show in light mode (#117)
    
    * Fix: remove asterisk
    
    * Fix: remove useless field. (#127)
    
    * Feat: Doc page v2 (#116)
    
    * Feat: Docs page v2
    
    * Feat: change default mode to light
    
    * fix: logo show in light mode (#117)
    
    * Fix: remove useless field
    
    * Fix: optimize the prompt
    
    * Fix: optimize the prompt

---
+++ src/pages/contact-us/Title/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+export default function (): React.ReactElement {
+  return <div className={styles.title}>contact us</div>;
+}
