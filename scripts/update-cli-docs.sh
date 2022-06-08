75b79ba10eb6379917c91c3ba30d25a156ffbb5e
Jinming Guo <vgjm456@qq.com>
2022-06-08 14:28:36 +0800

    fix: ci open pr  (#136)
    * fix: allow manually trigger
    
    * fix: ci open pr
    
    Signed-off-by: vgjm <vgjm456@qq.com>

--- scripts/update-cli-docs.sh
+++ scripts/update-cli-docs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-SOURCE_REPO=git@github.com:h8r-dev/heighliner.git
+SOURCE_REPO=https://github.com/h8r-dev/heighliner.git
 SOURCE_BRANCH=website-docs
 SOURCE_DIR=website-docs
 TARGET_DIR=docs/07-cli/hln/commands
@@ -8,7 +8,7 @@ HEIGHLINER_DIR=$(mktemp -d)
 
 git clone -b $SOURCE_BRANCH $SOURCE_REPO $HEIGHLINER_DIR
 rm $TARGET_DIR/*.md
-cp ../hln/$SOURCE_DIR/*.md $TARGET_DIR
+cp $HEIGHLINER_DIR/$SOURCE_DIR/*.md $TARGET_DIR
 rm -rf $HEIGHLINER_DIR
 
 echo cli docs updated
