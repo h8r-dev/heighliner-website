640a15a85b00a46e6ea84b631a004c8d599a604b
Jinming Guo <vgjm456@qq.com>
2022-06-08 13:14:08 +0800

    fix: update hln docs workflow (#134)
    * fix: update hln docs workflow
    
    Signed-off-by: vgjm <vgjm456@qq.com>
    
    * typo

---
+++ scripts/update-cli-docs.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SOURCE_REPO=git@github.com:h8r-dev/heighliner.git
+SOURCE_BRANCH=website-docs
+SOURCE_DIR=website-docs
+TARGET_DIR=docs/07-cli/hln/commands
+HEIGHLINER_DIR=$(mktemp -d)
+
+git clone -b $SOURCE_BRANCH $SOURCE_REPO $HEIGHLINER_DIR
+rm $TARGET_DIR/*.md
+cp ../hln/$SOURCE_DIR/*.md $TARGET_DIR
+rm -rf $HEIGHLINER_DIR
+
+echo cli docs updated
